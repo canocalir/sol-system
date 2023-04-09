fc4b79f6dcc3353f854ce5c339b70406730b101f
canocalir <can.ocalir@gmail.com>
2023-04-09 18:45:58 +0300

    added expandable planets container

--- src/components/BottomNavbar/BottomNavbar.styled.ts
+++ src/components/BottomNavbar/BottomNavbar.styled.ts
@@ -1,6 +1,16 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { flexWrapper } from "../../styles/globalStyles";
+import { BsChevronDoubleUp } from "react-icons/bs";
+
+const arrowDownAnimation = keyframes`
+0%{
+  top: 2rem;
+}
+100%{
+  top: 0rem;
+}
+`
 
 const NavbarPlanetsContainer = styled.div`
   ${flexWrapper("center")};
@@ -14,15 +24,14 @@ const NavbarPlanetsContainer = styled.div`
 
 const PlanetLink = styled(Link)`
   text-decoration: none;
-  ${flexWrapper('center')};
+  ${flexWrapper("center")};
   flex-direction: column;
   gap: 1rem;
-  h2{
+  h2 {
     color: #ffffff;
     text-transform: uppercase;
-    font-family: 'Bebas Neue', cursive;
+    font-family: "Bebas Neue", cursive;
     letter-spacing: 0.1rem;
-
   }
 `;
 
@@ -37,7 +46,7 @@ const NavbarPlanetCircle = styled.div<NavbarProps>`
   box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
     rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
     rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
-    &:hover{
+  &:hover {
     border: 3px solid #ffffff;
   }
   ${({ planet }) => {
@@ -54,14 +63,52 @@ const Heading = styled.h1`
   letter-spacing: 0.4rem;
   font-size: 3rem;
   font-weight: 600;
-  font-family: 'Bebas Neue', cursive;
+  font-family: "Bebas Neue", cursive;
   text-transform: uppercase;
+  ${flexWrapper("center")};
+  flex-direction: column;
+`;
+
+const DetailText = styled.h4`
+  color: #58c0bb;
+  font-size: 1rem;
+  border: 2px solid #58c0bb;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  cursor: pointer;
+  &:hover{
+    background-color: #58c0bb;
+    color: #000000;
+    padding: 0.5rem;
+    transition: 300ms ease-in-out;
+  }
+`;
+
+const DetailTextContainer = styled.div`
+  ${flexWrapper('center')};
+  flex-direction: column;
+  gap: 1rem;
+  padding-top: 1rem;
 `
 
 const NavbarContainer = styled.div`
   ${flexWrapper("center")};
   flex-direction: column;
-  gap: 1rem;
+  gap: 2rem;
+`;
+
+const UpArrow = styled(BsChevronDoubleUp)`
+  position: relative;
+  animation: ${arrowDownAnimation} 2s infinite;
 `
 
-export { NavbarContainer, NavbarPlanetsContainer, Heading,  PlanetLink, NavbarPlanetCircle };
+export {
+  NavbarContainer,
+  NavbarPlanetsContainer,
+  Heading,
+  DetailText,
+  PlanetLink,
+  UpArrow,
+  DetailTextContainer,
+  NavbarPlanetCircle,
+};
--- src/components/BottomNavbar/BottomNavbar.tsx
+++ src/components/BottomNavbar/BottomNavbar.tsx
@@ -1,6 +1,17 @@
-import { NavbarPlanetsContainer, NavbarContainer, Heading, NavbarPlanetCircle, PlanetLink } from "./BottomNavbar.styled";
+import { useState } from "react";
+import {
+  NavbarPlanetsContainer,
+  NavbarContainer,
+  Heading,
+  NavbarPlanetCircle,
+  PlanetLink,
+  DetailText,
+  DetailTextContainer,
+  UpArrow
+} from "./BottomNavbar.styled";
 
 const BottomNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const planets = [
     "neptune",
     "uranus",
@@ -11,19 +22,33 @@ const BottomNavbar = () => {
     "venus",
     "mercury",
   ];
-  return (
-    <NavbarContainer>
-    <Heading>Sol Planets</Heading>
+
+  const navbarShowConditional = isOpen && (
     <NavbarPlanetsContainer>
       {planets?.map((planet, index) => {
         return (
           <PlanetLink key={index} to={`/${planet}`}>
-            <NavbarPlanetCircle planet={planet}/>
+            <NavbarPlanetCircle planet={planet} />
             <h2>{planet}</h2>
           </PlanetLink>
         );
       })}
     </NavbarPlanetsContainer>
+  );
+
+  return (
+    <NavbarContainer>
+      <Heading>
+        Sol Planets
+        <DetailTextContainer>
+          <DetailText onClick={() => setIsOpen(!isOpen)}>
+            {!isOpen ? "Click here for details" : "Click here to close"}
+          </DetailText>
+          {!isOpen && <UpArrow />}
+        </DetailTextContainer>
+      </Heading>
+
+      {navbarShowConditional}
     </NavbarContainer>
   );
 };
